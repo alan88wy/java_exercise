c13eeb9a9eaa88466866d3a3ed6ab89f9878a56b
Alan Wan <alan88w@yahoo.com>
2025-03-27 20:46:57 +0800

    Added new Javafx Example

--- fx01/fx01/src/fx01/Controller.java
+++ fx01/fx01/src/fx01/Controller.java
@@ -2,6 +2,21 @@ package fx01;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
 
 public class Controller {
 
